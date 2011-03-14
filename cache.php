988c8334a721b14029fa606bee3764a6766d730b
Otar Chekurishvili <chekura@gmail.com>
2011-03-14 22:26:14 +0400

    Added PHP magic methods for quickly accessing and setting cache items. Denied direct object constructing and cloning.

--- cache.php
+++ cache.php
@@ -1,6 +1,6 @@
 <?php
 
-class CacheException extends Exception
+class Cache_Exception extends Exception
 {
 
 }
@@ -23,15 +23,30 @@ class Cache
         return $success ? $cache : $default;
     }
 
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
+
     public function set($key, $value, $expire = NULL)
     {
         NULL === $expire AND $expire = self::EXPIRE;
         return apc_store($this->_sanitize($key), $value, $expire);
     }
 
+    public function __set($key, $value)
+    {
+        return $this->set($key, $value);
+    }
+
     public function delete($key)
     {
         return apc_delete($this->_sanitize($key));
+    }
+
+    public function __unset($key)
+    {
+        return $this->delete($key);
     }
 
     public function clean()
@@ -42,13 +57,23 @@ class Cache
     protected static function _initialize()
     {
         if (!extension_loaded('apc') OR !function_exists('apc_store'))
-            throw new CacheException('Cache class requires PHP\'s native `APC` extension which is not installed or loaded.');
+            throw new Cache_Exception('Cache class requires PHP\'s native `APC` extension which is not installed or loaded.');
         self::$instance = new self;
     }
 
     protected function _sanitize($key)
     {
         return str_replace(array('/', '\\', ' '), '_', $key);
+    }
+
+    private function __construct()
+    {
+        throw new Cache_Exception('Directly creating the Cache object is not allowed, try Cache::instance() instead.');
+    }
+
+    private function __clone()
+    {
+        throw new Cache_Exception('Cloning the Cache objectis not allowed.');
     }
 
 }
